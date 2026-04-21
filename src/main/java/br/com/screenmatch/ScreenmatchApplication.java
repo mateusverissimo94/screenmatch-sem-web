9a81cee46ad5f4aaa9e38a8b784856b1fd7dd70e
Mateus Barros <mateusverissimo94@gmail.com>
2026-04-21 19:48:24 -0300

    modelando episódios

--- src/main/java/br/com/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,12 @@
 package br.com.screenmatch;
 
+import br.com.screenmatch.model.DadosEpisodio;
 import br.com.screenmatch.model.DadosSerie;
 import br.com.screenmatch.service.ConsumoAPI;
 import br.com.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.net.http.HttpClient;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -20,11 +19,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
 		var json =  consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=f5a33c67");
-//		System.out.println(json);
-//		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		DadosSerie serie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(serie);
+		json =  consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=f5a33c67");
+		DadosEpisodio episodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(episodio);
 	}
 }

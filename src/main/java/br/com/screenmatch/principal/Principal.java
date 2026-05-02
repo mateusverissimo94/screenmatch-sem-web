9eec1f3c9a862b083264f0e449a910f8d8e17b1b
Mateus Barros <mateusverissimo94@gmail.com>
2026-05-02 20:00:16 -0300

    episodios-por-ano

--- src/main/java/br/com/screenmatch/principal/Principal.java
+++ src/main/java/br/com/screenmatch/principal/Principal.java
@@ -37,6 +37,7 @@ public class Principal {
                     8 - Filtrar séries por temporadas e avaliação
                     9 - Buscar episódio por trecho
                     10 - Top 5 episódios por série
+                    11 - Buscar episódios a partir de uma data
                     
                     0 - sair
                     """;
@@ -75,6 +76,9 @@ public class Principal {
                     break;
                 case 10:
                     topEpisodiosPorSerie();
+                    break;
+                case 11:
+                    BuscarEpisodiosDepoisDeUmaData();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -210,6 +214,19 @@ public class Principal {
             topEpisodios.forEach(e ->
                     System.out.printf("Série: %s Temporada %s - Episódio %s - %s Avaliação %s\n",
                             e.getSerie().getTitulo(), e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao()));
+        }
+    }
+
+    private void BuscarEpisodiosDepoisDeUmaData() {
+        buscarSeriePorTitulo();
+        if (serieBusca.isPresent()) {
+            Serie serie = serieBusca.get();
+            System.out.println("Digite o ano limite de lançamento: ");
+            var anoLancamento = leitura.nextInt();
+            leitura.nextLine();
+
+            List<Episodio> episodiosAno = repositorio.episodiosPorSerieEAno(serie, anoLancamento);
+            episodiosAno.forEach(System.out::println);
         }
     }
 }

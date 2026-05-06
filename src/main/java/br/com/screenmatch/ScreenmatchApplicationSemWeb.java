01ebb36699e8b3c8e8d14c6bb6f979dfa3690e56
Mateus Barros <mateusverissimo94@gmail.com>
2026-05-06 12:26:42 -0300

    serie-controller-criada

---
+++ src/main/java/br/com/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,24 @@
+package br.com.screenmatch;
+
+import br.com.screenmatch.principal.Principal;
+import br.com.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
+//    @Autowired
+//    private SerieRepository repositorio;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Principal principal = new Principal(repositorio);
+//        principal.exibeMenu();
+//    }
+//}

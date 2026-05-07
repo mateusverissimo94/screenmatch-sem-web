ea244f074c41af054a5a8ff745ca4cdb3801dd58
Mateus Barros <mateusverissimo94@gmail.com>
2026-05-07 06:08:18 -0300

    cors-criado

--- src/main/java/br/com/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/screenmatch/controller/SerieController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestController

570535f66027f820b1913a395a50226c6933619b
Mateus Barros <mateusverissimo94@gmail.com>
2026-05-07 19:53:53 -0300

    top-5-controller

--- src/main/java/br/com/screenmatch/service/SerieService.java
+++ src/main/java/br/com/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.screenmatch.service;
 
 import br.com.screenmatch.dto.SerieDTO;
+import br.com.screenmatch.model.Serie;
 import br.com.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,17 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repositorio.findAll()
-                .stream()
+        return converteDados(repositorio.findAll());
+
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
                         s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());

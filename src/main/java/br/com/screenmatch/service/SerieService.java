a35f86f73d6511987a9e87cedcab8cf78b8cc33f
Mateus Barros <mateusverissimo94@gmail.com>
2026-05-07 18:46:31 -0300

    service-serie-criado

---
+++ src/main/java/br/com/screenmatch/service/SerieService.java
@@ -0,0 +1,23 @@
+package br.com.screenmatch.service;
+
+import br.com.screenmatch.dto.SerieDTO;
+import br.com.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+                        s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}
